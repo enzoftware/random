4df70b7880a20851332e6f5bf4f7db7189d2464a
Enzo Lizama Paredes <lizama.enzo@gmail.com>
2017-07-19 22:50:05 -0500

    How to think like a computer scientist

--- example.py
+++
@@ -1,4 +0,0 @@
-number1 = 1245
-number2 = 5454
-
-print(number1+number2)
--- hello.py
+++
@@ -1,3 +0,0 @@
-age = 18
-name = "enzoftware"
-print("hello world my name is "+name + " and I have " + str(age) +  " years")
