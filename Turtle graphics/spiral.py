7f33809521dd30a0935bbdcb83b7e751b77ad0f3
Enzo Lizama Paredes <lizama.enzo@gmail.com>
2017-07-21 22:54:00 -0500

    spiral

--- Turtle graphics/spiral.py
+++ Turtle graphics/spiral.py
@@ -10,6 +10,6 @@ tess.up()                     # this is new
 for size in range(5, 60, 2):    # start with size = 5 and grow by 2
     tess.stamp()                # leave an impression on the canvas
     tess.forward(size)          # move tess along
-    tess.right(25)              # and turn her
+    tess.right(26)              # and turn her
 
 wn.exitonclick()
