0c2c619b6c9d94cc1c05d5708b576afdc6f4d02b
enzoftware <lizama.enzo@gmail.com>
2018-02-13 21:34:07 -0500

    Mobile Vision API - barcode reader

---
+++ Kotlin/BarcodeReader/app/src/main/java/com/experiments/enzoftware/barcode_reader_example/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.experiments.enzoftware.barcode_reader_example
+
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.util.SparseArray
+import com.google.android.gms.vision.Frame
+import com.google.android.gms.vision.barcode.Barcode
+import com.google.android.gms.vision.barcode.BarcodeDetector
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val myBitmap : Bitmap = BitmapFactory.decodeResource(applicationContext.resources,R.drawable.barcode)
+        imgview.setImageBitmap(myBitmap)
+
+        button.setOnClickListener {
+
+            val detector = BarcodeDetector.Builder(applicationContext)
+                    .setBarcodeFormats(Barcode.ALL_FORMATS)
+                    .build()
+            if (!detector.isOperational){
+                txtContent.text = "Could not setup the detector"
+            }
+
+            val frame = Frame.Builder().setBitmap(myBitmap).build()
+            val barcodeList : SparseArray<Barcode> = detector.detect(frame)
+            Log.i("barcode",barcodeList.valueAt(0).rawValue)
+            val thisCode = barcodeList.valueAt(0)
+            txtContent.text = thisCode.rawValue
+        }
+    }
+}
